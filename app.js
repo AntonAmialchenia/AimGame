def17440144da4b08ebe4b9f38c2cc091361532e
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2022-11-11 12:40:32 +0300

    мой проект

---
+++ app.js
@@ -0,0 +1,101 @@
+const startBtn = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
+const timeList = document.querySelector('#time-list');
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('#board');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71','#9a463d', '#56286a', '#24597c',  '#58371a', '#086e33'];
+
+let time = 0;
+let score = 0;
+
+startBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    screens[0].classList.add('up');
+});
+
+timeList.addEventListener('click', event => 
+{
+    if (event.target.classList.contains('time-btn')) {
+        time = parseInt(event.target.getAttribute('data-time'));
+        screens[1].classList.add('up');
+        startGame();
+    }
+});
+
+board.addEventListener('click', event => {
+    if (event.target.classList.contains('circle')) {
+        score++;
+        
+        event.target.remove();
+        createRandomCircle();
+        
+
+    }
+});
+
+
+function startGame() {
+    setInterval(decreaseTime,1000);
+    createRandomCircle();
+    setTime(time);
+}
+
+function decreaseTime() {
+    if (time === 0) {
+        finishGame();
+    } else {
+    let current = --time;
+    if (current < 10) {
+        current = `0${current}`;
+    }
+    setTime(current);
+    }
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`;
+}
+
+function finishGame() {
+    timeEl.parentNode.classList.add('hide');
+    board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div');
+    const size = getRandomNumebr(10,60);
+    const {width, height} = board.getBoundingClientRect();
+    const color = getRandomColor();
+    
+    const x = getRandomNumebr(0, width - size);
+    const y = getRandomNumebr(0, height - size);
+
+    circle.classList.add('circle');
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    circle.style.top = `${y}px`;
+    circle.style.left = `${x}px`;
+    circle.style.backgroundColor = color;
+    circle.style.boxShadow = `0 0 5px ${color}, 0 0 20px ${color}`
+    
+    board.append(circle);
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function getRandomNumebr(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+// Хак игры, вызвать в консоле после выбора времени
+function winTheGame() {
+    function kill() {
+        const circle = document.querySelector('.circle');
+        if (circle) {
+            circle.click();    
+        }     
+    }
+    setInterval(kill, 75);
+}
